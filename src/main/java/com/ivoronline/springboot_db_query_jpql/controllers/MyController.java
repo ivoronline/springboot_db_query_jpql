2175c9c501a5724b0687eefbfaa4085c9144b28e
igoreta <ivoronline@gmail.com>
2021-08-27 14:25:58 +0200

    COmmit

--- src/main/java/com/ivoronline/springboot_db_query_jpql/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql/controllers/MyController.java
@@ -5,10 +5,7 @@ import com.ivoronline.springboot_db_query_jpql.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,75 +15,76 @@ public class MyController {
   @Autowired PersonRepository personRepository;
 
   //================================================================
-  // SELECT JOHN
+  // NO PARAMETERS
   //================================================================
-  @ResponseBody
-  @RequestMapping("/SelectJohn")
-  public Person selectJohn() {
-    Person john = personRepository.selectJohn();
+  @RequestMapping("NoParameters")
+  Person noParameters() {
+    Person person = personRepository.noParameters();
+    return person;
+  }
+
+  //================================================================
+  // INDEXED PARAMETERS
+  //================================================================
+  @RequestMapping("IndexedParameters")
+  Person indexedParameters() {
+    Person person = personRepository.indexedParameters("John", 20);
+    return person;
+  }
+
+  //================================================================
+  // NAMED PARAMETERS
+  //================================================================
+  @RequestMapping("NamedParameters")
+  Person namedParameters() {
+    Person person = personRepository.namedParameters("John", 20);
+    return person;
+  }
+
+  //================================================================
+  // SELECT PERSON BY NAME AGE (Returns Single Entity)
+  //================================================================
+  @RequestMapping("SelectPersonByNameAge")
+  Person selectPersonByNameAge() {
+    Person john = personRepository.selectPersonByNameAge("John", 20);
     return john;
   }
 
   //================================================================
-  // SELECT PERSON BY NAME AGE INDEXED
+  // SELECT PERSONS BY NAME (Returns List)
   //================================================================
-  @ResponseBody
-  @RequestMapping("/SelectPersonByNameAgeIndexed")
-  public Person selectPersonByNameAgeIndexed(@RequestParam String name, @RequestParam Integer age) {
-    Person person = personRepository.selectPersonByNameAgeIndexed(name, age);
-    return person;
-  }
-
-  //================================================================
-  // SELECT PERSON BY NAME AGE NAMED
-  //================================================================
-  @ResponseBody
-  @RequestMapping("/SelectPersonByNameAgeNamed")
-  public Person selectPersonByNameAgeNamed(@RequestParam String name, @RequestParam Integer age) {
-    Person person = personRepository.selectPersonByNameAgeNamed(name, age);
-    return person;
-  }
-
-  //================================================================
-  // SELECT PERSONS BY NAME
-  //================================================================
-  @ResponseBody
-  @RequestMapping("/SelectPersonsByName")
-  public List<Person> selectPersonsByName(@RequestParam String name) {
-    List<Person> persons = personRepository.selectPersonsByName(name);
+  @RequestMapping("SelectPersonsByName")
+  List<Person> selectPersonsByName() {
+    List<Person> persons = personRepository.selectPersonsByName("John");
     return persons;
   }
 
   //================================================================
   // SELECT PERSONS BY NAME SORTED
   //================================================================
-  @ResponseBody
-  @RequestMapping("/SelectPersonsByNameSorted")
-  public List<Person> selectPersonsByNameSorted(@RequestParam String name) {
-    List<Person> persons = personRepository.selectPersonsByNameSorted(name, Sort.by("age"));
+  @RequestMapping("SelectPersonsByNameSorted")
+  List<Person> selectPersonsByNameSorted() {
+    List<Person> persons = personRepository.selectPersonsByNameSorted("John", Sort.by("age"));
     return persons;
   }
 
   //================================================================
   // UPDATE PERSON BY NAME
   //================================================================
-  @ResponseBody
-  @Transactional
-  @RequestMapping("/UpdatePersonsByName")
-  public String updatePersonsByName(@RequestParam String name, @RequestParam Integer newAge) {
-    Integer recordsUpdated = personRepository.updatePersonsByName(name, newAge);
-    return recordsUpdated + " Records were updated";
+  @RequestMapping("UpdatePersonsByName")
+  String updatePersonsByName() {
+    Integer recordsUpdated = personRepository.updatePersonsByName("John", 50); //New age
+    return  recordsUpdated + " Records Updated";
   }
 
   //================================================================
   // DELETE PERSON BY NAME
   //================================================================
-  @ResponseBody
-  @Transactional
-  @RequestMapping("/DeletePersonsByName")
-  public String deletePersonsByName(@RequestParam String name) {
-    Integer recordsDeleted = personRepository.deletePersonsByName(name);
-    return recordsDeleted + " Records were deleted";
+  @RequestMapping("DeletePersonsByName")
+  String deletePersonsByName() {
+
+    Integer recordsDeleted = personRepository.deletePersonsByName("John");
+    return  recordsDeleted + " Records Deleted";
   }
 
 }
--- src/main/java/com/ivoronline/springboot_db_query_jpql/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql/repositories/PersonRepository.java
@@ -6,26 +6,32 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
   //=======================================================================================
-  // SELECT
+  // PARAMETERS
   //=======================================================================================
-
   //NO PARAMETERS
   @Query(value = "SELECT john FROM Person john WHERE john.name = 'John' AND john.age = 20")
-  Person selectJohn();
+  Person noParameters();
 
   //INDEXED PARAMETERS
   @Query(value = "SELECT person FROM Person person WHERE person.name = ?1 AND person.age = ?2")
-  Person selectPersonByNameAgeIndexed(String name, Integer age);
+  Person indexedParameters(String name, Integer age);
 
   //NAMED PARAMETERS
   @Query(value = "SELECT person FROM Person person WHERE person.name = :name AND person.age = :parameterAge")
-  Person selectPersonByNameAgeNamed(String name, @Param("parameterAge") Integer age);
+  Person namedParameters(String name, @Param("parameterAge") Integer age);
+
+  //=======================================================================================
+  // SELECT
+  //=======================================================================================
+  //RETURN SINGLE ENTITY
+  @Query(value = "SELECT john FROM Person john WHERE john.name = :name AND john.age = :age")
+  Person selectPersonByNameAge(String name, Integer age);
 
   //RETURN LIST
   @Query(value = "SELECT person FROM Person person WHERE person.name = :name")
@@ -39,6 +45,7 @@ public interface PersonRepository extends CrudRepository<Person, Integer> {
   // UPDATE
   //=======================================================================================
   @Modifying
+  @Transactional
   @Query(value = "UPDATE Person person SET person.age = :newAge WHERE person.name = :name")
   Integer updatePersonsByName(String name, Integer newAge);
 
@@ -46,11 +53,12 @@ public interface PersonRepository extends CrudRepository<Person, Integer> {
   // DELETE
   //=======================================================================================
   @Modifying
+  @Transactional
   @Query(value = "DELETE FROM Person person WHERE person.name = :name")
   Integer deletePersonsByName(String name);
 
   //=======================================================================================
-  // INSERT IS NOT SUPPORTED BY JPA
+  // INSERT (NOT SUPPORTED BY JPA)
   //=======================================================================================
 
 }
