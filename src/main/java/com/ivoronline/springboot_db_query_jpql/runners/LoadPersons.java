6746193f5f3fc3d1fbc7209ff80e2878441ff25b
igoreta <ivoronline@gmail.com>
2021-08-29 07:13:54 +0200

    COmmit

--- src/main/java/com/ivoronline/springboot_db_query_jpql/runners/LoadPersons.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql/runners/LoadPersons.java
@@ -18,7 +18,7 @@ public class LoadPersons implements CommandLineRunner {
   @Transactional
   public void run(String... args) throws Exception {
     personRepository.save(new Person("John" , 20));
-    personRepository.save(new Person("John" , 11));
+    personRepository.save(new Person("John" , 21));
     personRepository.save(new Person("Bill" , 30));
     personRepository.save(new Person("Nancy", 40));
     personRepository.save(new Person("Susan", 50));
