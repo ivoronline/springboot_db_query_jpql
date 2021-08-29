b7799340e586be811baf2dc5787a3b23863da5d7
igoreta <ivoronline@gmail.com>
2021-08-29 07:15:11 +0200

    COmmit

--- src/main/java/com/ivoronline/springboot_db_query_jpql/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql/controllers/MyController.java
@@ -1,11 +1,9 @@
 package com.ivoronline.springboot_db_query_jpql.controllers;
 
-import com.ivoronline.springboot_db_query_jpql.dto.PersonDTO;
 import com.ivoronline.springboot_db_query_jpql.entities.Person;
 import com.ivoronline.springboot_db_query_jpql.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
